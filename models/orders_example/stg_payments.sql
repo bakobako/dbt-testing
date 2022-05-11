97e9cd3117f6e33f9432fb90bfbdda2627b1b52a
Adam Bako <bakoad@gmail.com>
2022-05-11 17:07:46 +0200

    Update stg_payments.sql

--- models/orders_example/stg_payments.sql
+++ models/orders_example/stg_payments.sql
@@ -9,8 +9,6 @@ renamed as (
         id as payment_id,
         order_id,
         payment_method,
-
-        -- `amount` is currently stored in cents, so we convert it to dollars
         amount / 100 as amount
 
     from source
