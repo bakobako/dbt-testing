23869d30ac602d5c4217f22955ddfa1dfb4dade7
Adam Bako <bakoad@gmail.com>
2022-05-11 16:25:10 +0200

    update usecase to jafa

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,17 @@
+with source as (
+    select * from {{ source('my_source', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        id as customer_id,
+        first_name,
+        last_name
+
+    from source
+
+)
+
+select * from renamed
